a2f44671da25bf9e7a1a4a9fff799ebd1ee08af9
jhonedw573 <vaj573cali@gmail.com>
2025-10-12 23:24:50 -0500

    cambio en vercely en db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,23 @@
-// config/db.js
-import mongoose from 'mongoose';
+// // config/db.js
+// import mongoose from 'mongoose';
 
-export const conectarDb = async() =>{
-    try {
-      await mongoose.connect(process.env.MONGO_URI)
-        console.log("Conectado a la base de datos correctamente")
-    } catch (error) {
-        console.log("No se pudo conectar a la base de datos")
-    }
+// export const conectarDb = async() =>{
+//     try {
+//       await mongoose.connect(process.env.MONGO_URI)
+//         console.log("Conectado a la base de datos correctamente")
+//     } catch (error) {
+//         console.log("No se pudo conectar a la base de datos")
+//     }
+// }
+
+
+
+export const conectarDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("✅ Conectado a la base de datos correctamente")
+  } catch (error) {
+    console.error("❌ Error de conexión a MongoDB:", error.message)
+    throw error // <- esto hará que el log aparezca en Vercel
+  }
 }
