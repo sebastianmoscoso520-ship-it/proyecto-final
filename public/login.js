20ca4b40f906b6fe85164627d789c3d8dac6a7fa
jhonedw573 <vaj573cali@gmail.com>
2025-10-12 20:10:40 -0500

    hice cambios para que funcionara con versel

--- public/login.js
+++ public/login.js
@@ -1,4 +1,5 @@
-const API = 'http://localhost:8000/api/auth';
+// const API = 'http://localhost:8000/api/auth';
+const API= "https://aplicacionAutenticacionEinventario.vercel.app/api/auth"; // es para utilizar con versel
 const out = document.getElementById('out');
 
 document.getElementById('loginForm').addEventListener('submit', async e => {
